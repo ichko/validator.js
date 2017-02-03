e6bb872175686464ee418d40517ede72787af962
Iliya Zhechev <i4ko95@gmail.com>
2017-02-03 21:38:59 +0000

    Reffactoring

--- validator.js
+++ validator.js
@@ -1,42 +1,42 @@
 class Validator {
-    
+
     constructor() {
         this.validations = { };
         this.extractors = { };
         this.effects = { };
     }
-    
+
     validation(name, ...rules) {
-        this.rules[name] = rules;
+        this.validations[name] = rules;
         return this;
     }
-    
+
     extractor(extractor) {
         this.extractors[extractor.name] = extractor;
-        return this
+        return this;
     }
-    
+
     effect (name, ...effects) {
         this.effects[name] = effects;
-        return this
+        return this;
     }
-    
+
     validate({ elements, validation, effect, dependencies, extractor = (x => x) }) {
         elements.forEach(element => {
             let errors = [];
-            
+
             this.validations[validation].forEach(rule => {
                 let value = extractor(element, dependencies);
                 if (!rule.handler(value, dependencies)) {
                     errors.push(rule.message(value));
                 }
             });
-            
-            let effectHandlerName = errors.length > 0 ? 'success' : 'error';
+
+            let effectHandlerName = errors.length == 0 ? 'success' : 'error';
             this.effects[effect].forEach(effect => {
-                effect[effect](element, errors, dependencies);
+                effect[effectHandlerName](element, errors, dependencies);
             });
         });
     }
-    
+
 }
